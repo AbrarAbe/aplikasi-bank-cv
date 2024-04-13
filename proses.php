f8add1438fc250ba70ad8509d1c43d45cbb07266
AbrarAbe <cadakunmail@gmail.com>
2024-04-13 13:46:09 +0700

    Proses upload CV dan pas foto

--- proses.php
+++ proses.php
@@ -23,12 +23,26 @@
         $universitas = $_POST['universitas'];
         $pengalaman = $_POST['pengalaman'];
         $deskripsi = $_POST['deskripsi'];
+        
+        // Upload file cv dan foto
+        $cvData = file_get_contents($_FILES['cv']['tmp_name']);
+        $fotoData = file_get_contents($_FILES['foto']['tmp_name']);
+
+        // Insert file uploads into file_uploads table
+        $sql = $koneksi->prepare("INSERT INTO dokumen (cv, foto) VALUES (?, ?)");
+        $sql->bind_param("ss", $cvData, $fotoData);
+        $sql->execute();
+        $fileUploadId = $sql->insert_id;
+        $sql->close();
 
         // Menyiapkan pernyataan SQL untuk menyisipkan data
-        $sql = "INSERT INTO data_pelamar (nama, email, alamat, telepon, pendidikan, universitas, pengalaman, deskripsi) VALUES ('$nama', '$email', '$alamat', '$telepon', '$pendidikan', '$universitas', '$pengalaman', '$deskripsi')";
+        $sql = $koneksi->prepare("INSERT INTO data_pelamar (nama, email, alamat, telepon, pendidikan, universitas, pengalaman, deskripsi, cv_id, foto_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        // Sambung variabel berdasdarkan tipe data(s=string, i=int)
+        $sql->bind_param("ssssssisii", $nama, $email, $alamat, $telepon, $pendidikan, $universitas, $pengalaman, $deskripsi, $fileUploadId, $fileUploadId);
+        $sql->execute();
 
         // Menjalankan pernyataan SQL dan memeriksa keberhasilannya
-        if ($koneksi->query($sql) === TRUE) {
+        if ($sql->affected_rows > 0) {
             echo "Data berhasil disimpan ke database. <a class='button' href='index.php' style='margin-left:210px'>Kembali ke halaman Utama</a> <a class='button1' href='view_data.php'>Lihat Data</a>";
             // Redirect ke halaman index.php setelah proses penyimpanan data selesai
         
